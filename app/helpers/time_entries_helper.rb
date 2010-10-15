9e35e12d92e5c238bab7e5bb32a0395f23e2e739
Brian Pokosh <bpokosh@gmail.com>
2010-10-15 11:43:03 -0400

    added time picker

--- app/helpers/time_entries_helper.rb
+++ app/helpers/time_entries_helper.rb
@@ -1,2 +1,5 @@
 module TimeEntriesHelper
+  def format_time(duration)
+    duration.to_s + " minutes"
+  end
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -3,4 +3,10 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :employee
   validates :project_id, :presence => true
   validates :employee_id, :presence => true
+  
+  before_save :calculate_duration
+  
+  def calculate_duration
+    self.duration = (self.end_time - self.start_time) / 60
+  end
 end
