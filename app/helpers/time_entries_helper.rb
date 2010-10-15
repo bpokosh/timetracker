0f1338448e553c0d30c9e23b9954c2a9f161c7a8
Brian Pokosh <bpokosh@gmail.com>
2010-10-15 12:53:15 -0400

    clean up

--- app/helpers/time_entries_helper.rb
+++ app/helpers/time_entries_helper.rb
@@ -1,5 +1,13 @@
 module TimeEntriesHelper
   def format_time(duration)
-    duration.to_s + " minutes"
+    if duration < 60
+      pluralize(duration, 'minutes')
+    else
+      hours = duration / 60
+      minutes = duration % 60
+      output = pluralize(hours, 'hours')
+      output += ", " + pluralize(minutes, 'minutes') if minutes != 0
+      output
+    end
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
   has_many :projects, :dependent => :destroy
+  has_many :time_entries, :through => :projects
 end
